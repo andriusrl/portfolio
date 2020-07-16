3fbff9f645acf7937eec439fb213502b8f490edb
Andrius <andrius.rochalazarino@gmail.com>
2020-07-16 15:03:41 -0400

    adicinado icones das redes sociais

--- src/style/style.js
+++ src/style/style.js
@@ -4,10 +4,43 @@ export const TopBioContainer = styled.div`
       width: 564px;
       height: 621px;
       background-color: #011726;
+      display: flex;
+      justify-content: right;
+`
+
+export const WrapperBioContainer = styled.div`
+    display: flex;
+`
+
+export const Menu = styled.div`
+    position: absolute;
+    margin: 3% 6% 0 0;
+    color: #ffffff;
+    z-index: 1;
+
+`
+export const Name = styled.div`
+    /* justify-self: l; */
+    /* margin: 3% 0 0 0; */
+    margin-right: auto;
+    color: #ffffff;
 `
 
 export const Photo = styled.img`
-    width: 100%;
-    height: 100%;
+    position: absolute;
+    width: 564px;
+    height: 621px;
     opacity: 0.6;
+`
+
+export const SocialNetworksWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: auto 16% auto 0;
+    z-index: 1;
+`
+
+export const SocialNetwrokIcon = styled.img`
+    margin: 16%;
+    width: 39px;
 `
