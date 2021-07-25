6ac1f6c1d35dc21aeeb9b0bd11167a78bc28150e
Andrius <andrius.rochalazarino@gmail.com>
2021-07-25 18:28:05 -0400

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -57,7 +57,7 @@ function App() {
           </h3>
             <p>
               A princípio gostava de editar código de servidores de jogos online depois passei a tentar fazer pequenos jogos apenas por curiosidade e acabei conhecendo outras linguagens que tive um pequeno contato como C, C++, C#, java, mas foi o desenvolvimento web que mais me chamou atenção pela quantidade tecnologias e pessoas envolvidas.
-              Atualmente estou cursando o 6° semestre de engenharia de software e também finalizando outro curso fullstack de desenvolvimento web onde participei de diversos trabalhos em equipe.
+              Atualmente estou cursando o 8° semestre de engenharia de software.
           </p>
             <p>
               JavaScript | React | Node | MySQL | HTML | CSS
